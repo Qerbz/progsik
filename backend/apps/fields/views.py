d12f3960ba6f3da91d7055a24f02d40175e3353d
John Hausberg Anfindsen <john.h.anfindsen@gmail.com>
2024-03-08 12:53:11 +0100

    Fix WSTG-INPV-05

--- backend/apps/fields/views.py
+++ backend/apps/fields/views.py
@@ -46,10 +46,10 @@ class FieldBookingsView(APIView):
         start_time = local_date_obj
         end_time = start_time + timedelta(days=1)
 
-        # Unsafe and vulnerable SQL query
-        raw_query = f"SELECT * FROM fields_booking WHERE field_id = {field_id} AND '{start_time}' <= end_time AND '{end_time}' >= start_time"
+        # Safe, parameterized SQL query
+        query = "SELECT * FROM fields_booking WHERE field_id = %s AND %s <= end_time AND %s >= start_time"
 
-        bookings = Booking.objects.raw(raw_query)
+        bookings = Booking.objects.raw(query, [field_id, start_time, end_time])
 
         serializer = BookingSerializer(bookings, many=True)
         return Response(serializer.data)
